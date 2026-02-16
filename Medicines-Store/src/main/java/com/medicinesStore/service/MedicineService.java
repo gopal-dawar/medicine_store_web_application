1b62e709b0745f406eba868a476675807ca9a925
gopal-dawar <gopaldawar45@gmail.com>
2026-02-16 14:29:10 +0530

    medicines bussiness logic and all rest api of medicines class

--- Medicines-Store/src/main/java/com/medicinesStore/service/MedicineService.java
+++ Medicines-Store/src/main/java/com/medicinesStore/service/MedicineService.java
@@ -1,10 +1,20 @@
 package com.medicinesStore.service;
 
 import com.medicinesStore.entity.Medicines;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import java.util.List;
 
 public interface MedicineService {
-    Medicines saveMedicine(Medicines medicine, MultipartFile file) throws IOException;
+
+    Medicines addMedicine(Medicines medicine);
+
+    Medicines updateMedicine(Long medicineId, Medicines medicine);
+
+    Medicines getMedicineById(Long medicineId);
+
+    List<Medicines> getAllMedicines();
+
+    List<Medicines> searchMedicineByName(String name);
+
+    void deleteMedicine(Long medicineId);
 }
